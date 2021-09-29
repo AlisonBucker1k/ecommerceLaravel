58be0dc303c2bb1aece0f21d1b79522922ed2612
Gabriel Bitti <gabrielbitti0@gmail.com>
2021-09-29 01:49:53 -0300

    Corrige listagem e criação de endereços

--- app/Rules/CepValidate.php
+++ app/Rules/CepValidate.php
@@ -2,39 +2,27 @@
 
 namespace App\Rules;
 
+use Exception;
 use Illuminate\Contracts\Validation\Rule;
 
 class CepValidate implements Rule
 {
     /**
-     * Create a new rule instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
      * Determine if the validation rule passes.
      *
-     * @param  string  $attribute
-     * @param  mixed  $value
+     * @param string $attribute
+     * @param mixed $value
      * @return bool
      */
     public function passes($attribute, $value)
     {
         try {
             $info = zipcode($value);
-            if (empty($info)) {
-                return false;
-            }
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             return false;
         }
 
-        return true;
+        return empty($info);   
     }
 
     /**
