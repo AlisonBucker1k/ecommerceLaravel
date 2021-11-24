6b6ae62bd162b6161440b42dcb6a2d7b4e46dcba
Gabriel Bitti <gabrielbitti0@gmail.com>
2021-11-23 23:49:35 -0300

    Corrige post-back

--- app/Payments/PagarMe/Order.php
+++ app/Payments/PagarMe/Order.php
@@ -34,8 +34,12 @@ class Order extends PagarMe
 
     private function validatePostBackSignature(): void
     {
-        $requestBody = file_get_contents('php://input');
         $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+        if (empty($signature)) {
+            throw new Exception('Assinatura inválida.');
+        }
+
+        $requestBody = file_get_contents('php://input');
 
         $pagarMe = new Client(config('app.pagar_me_api_token'));
         $isValidPostback = $pagarMe->postbacks()->validate($requestBody, $signature);
@@ -49,10 +53,10 @@ class Order extends PagarMe
         $this->validatePostBackSignature();
 
         if (!$postBackResponse->status == 'success') {
-            throw new Exception('Erro no post-back do PagarMe.');
+            throw new Exception('Erro no post-back: PagarMe.');
         }
 
-        $order = UseLadameOrder::getFromPagarMeTransactionId($postBackResponse->model_id);
+        $order = UseLadameOrder::getFromPagarMeTransactionId($postBackResponse->transaction->id);
         if (!empty($order) && !empty($order->pagar_me_json)) {
             $pagarMeDecodedJson = json_decode($order->pagar_me_json);
             $pagarMeDecodedJson->status = $postBackResponse->payload->current_status;
